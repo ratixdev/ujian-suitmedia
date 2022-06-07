7972910c55e31c80c74ca0c4bb779b5aa4cf71b4
Ferry Ananda Febian <ratix@ratix>
2022-06-07 05:18:13 -0400

    upload task test

---
+++ test/app.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');describe('Test app', () => {
+ it('should return 2', () => {
+        assert.equal(1 + 1, 2);
+    });
+ it('should return 9', () => {
+        assert.equal(3 * 3, 9);
+    });
+});
